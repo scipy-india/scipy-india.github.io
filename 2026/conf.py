82d394ad07199306ab066906998e6c78f74b9b15
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2026-07-31 12:43:56 +0530

    Commit the rest of the 2026 config

--- 2026/conf.py
+++ 2026/conf.py
@@ -1,3 +1,31 @@
+"""
+Sphinx configuration for the SciPy India 2026 conference website.
+
+Usage:
+
+Build both sites, and build the main website first:
+
+    uv run sphinx-build -b html docs _build/html
+    uv run sphinx-build -b html 2026 _build/html/2026
+"""
+
+project = "SciPy India 2026 conference"
+html_title = "SciPy India 2026 conference"
+copyright = "2026, The SciPy India team"
+author = "The SciPy India team"
+
+html_baseurl = "https://scipy.in/2026/"
+
+html_theme = "pydata_sphinx_theme"
+
+extensions = [
+    "myst_parser",
+    "ablog",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "sphinx_togglebutton",
+]
+
 html_static_path = ["_static"]
 _FLIPDOWN_CSS = "https://unpkg.com/flipdown@0.3.2/dist/flipdown.min.css"
 _FLIPDOWN_JS = "https://unpkg.com/flipdown@0.3.2/dist/flipdown.min.js"
@@ -23,6 +51,8 @@ html_js_files = [
         },
     ),
 ]
+templates_path = ["_templates"]
+
 html_context = {
     "conference_nav": [
         {
@@ -56,3 +86,99 @@ html_context = {
         {"label": "Past editions", "page": "https://scipy.in/past-editions"},
     ],
 }
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
+html_show_sourcelink = False
+
+html_sidebars = {
+    "index": [],
+    "programme": [],
+    "speakers": [],
+    "cfp": [],
+    "register": [],
+    "venue": [],
+    "sponsor": [],
+    "faq": [],
+    "financial-aid": [],
+    "get-involved": [],
+    "coc": [],
+    "team": [],
+    "news/**": ["ablog/postcard.html", "ablog/recentposts.html", "ablog/tagcloud.html"],
+}
+
+myst_enable_extensions = ["colon_fence", "deflist", "attrs_inline"]
+
+html_logo = "_static/logo.svg"
+html_favicon = "_static/logo.svg"
+
+html_theme_options = {
+    "logo": {
+        "image_light": "_static/logo.svg",
+        "image_dark": "_static/logo.svg",
+        "alt_text": "SciPy India 2026 logo",
+        "text": "SciPy India 2026",
+    },
+    "navbar_start": ["navbar-logo"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["navbar-icon-links", "theme-switcher"],
+    "navbar_persistent": ["search-button"],
+    "footer_start": ["copyright"],
+    "footer_end": ["past-editions"],
+    "show_prev_next": False,
+    "secondary_sidebar_items": ["page-toc"],
+    "icon_links": [
+        {
+            "name": "Email",
+            "url": "mailto:info@scipy.in",
+            "icon": "fa-solid fa-envelope",
+            "type": "fontawesome",
+        },
+        {
+            "name": "News feed",
+            "url": "https://scipy.in/2026/news/atom.xml",
+            "icon": "fa-solid fa-rss",
+            "type": "fontawesome",
+        },
+        {
+            "name": "YouTube",
+            "url": "https://www.youtube.com/@scipy-india",
+            "icon": "fa-brands fa-youtube",
+            "type": "fontawesome",
+        },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/scipy-india",
+            "icon": "fab fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Zulip",
+            "url": "https://scipyindia.zulipchat.com/join/4mesdxfbbpl4titgtdzx4iwv/",
+            "icon": "fa-brands fa-zulip",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Bluesky",
+            "url": "https://bsky.app/profile/scipyindia.bsky.social",
+            "icon": "fa-brands fa-bluesky",
+            "type": "fontawesome",
+        },
+        {
+            "name": "LinkedIn",
+            "url": "https://www.linkedin.com/company/scipyindia",
+            "icon": "fab fa-linkedin",
+            "type": "fontawesome",
+        },
+    ],
+    "use_edit_page_button": False,
+    "show_toc_level": 2,
+    "navigation_with_keys": True,
+    "back_to_top_button": True,
+}
+
+# ABlog settings
+blog_path = "news"
+blog_title = "SciPy India 2026 news"
+blog_baseurl = "https://scipy.in/2026"
+blog_feed_fulltext = True
+blog_post_pattern = "news/*.md"
+post_date_format = "%d %B %Y"
